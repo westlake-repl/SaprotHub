5c702168bf40e74ec082ac05aee5ddde84a2d64e
LTEnjoy <sujinltenjoy@gmail.com>
2024-07-09 18:58:45 +0800

    Update

--- saprot/model/saprot/base.py
+++ saprot/model/saprot/base.py
@@ -365,6 +365,7 @@ class SaprotBaseModel(AbstractModel):
         
         with self.grid.output_to(0, 0):
             self.grid.clear_cell()
+            print(111111111111111111)
             
             fig = plt.figure(figsize=(6 * len(log_dict), 6))
             ax = []
