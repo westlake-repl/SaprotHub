ae79e63a6e1b1dacd702199106796bc66e766894
LTEnjoy <sujinltenjoy@gmail.com>
2024-07-09 17:30:51 +0800

    Update

--- local_server/run.sh
+++ local_server/run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Checkout the version
-ori_dir=$(pwd)
-cd /root/.cache/SaprotHub/SaprotHub
-local=$(git rev-parse HEAD)
-remote=$(git ls-remote https://github.com/westlake-repl/SaprotHub.git | grep HEAD)
-remote=(${remote//,/ }[0])
-if [ "$local" != "$remote" ]; then
-    echo "The version is not the latest. Updating..."
-    git pull
-    pip install -r ./requirements.txt
-
-fi
-local=$(git rev-parse HEAD)
+#ori_dir=$(pwd)
+#cd /root/.cache/SaprotHub/SaprotHub
+#local=$(git rev-parse HEAD)
+#remote=$(git ls-remote https://github.com/westlake-repl/SaprotHub.git | grep HEAD)
+#remote=(${remote//,/ }[0])
+#if [ "$local" != "$remote" ]; then
+#    echo "The version is not the latest. Updating..."
+#    git pull
+#    pip install -r ./requirements.txt
+#
+#fi
+#local=$(git rev-parse HEAD)
 
 # Run the server
 source activate SaprotHub
