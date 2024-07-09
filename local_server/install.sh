a21af409e0f8badfc14a1e05d24ac30ec15f8484
LTEnjoy <sujinltenjoy@gmail.com>
2024-07-09 12:14:41 +0800

    Update

--- local_server/install.sh
+++ local_server/install.sh
@@ -7,24 +7,7 @@ mkdir -p /root/.cache/SaprotHub
 # Install packages
 git clone https://github.com/googlecolab/colabtools.git /root/.cache/SaprotHub/colabtools
 pip install /root/.cache/SaprotHub/colabtools/
-pip install traitlets==5.9.0
-pip install pillow==10.4.0
-pip install httplib2==0.22.0
-pip install ipywidgets==7.7.1
-pip install 'numpy<2'
-pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
-pip install pytorch-lightning==2.1.3
-pip install loguru==0.7.2
-pip install easydict
-pip install colorama==0.4.6
-pip install transformers==4.28.0
-pip install peft==0.10.0
-pip install lmdb==1.4.1
-pip install biopython==1.83
-pip install wandb==0.17.4
-pip install matplotlib==3.9.1
-pip install torchmetrics==0.9.3
-pip install multiprocess
+pip install -r ./requirements.txt
 
 # Overwrite files for properly calling colab
 colab_dir=$CONDA_PREFIX/lib/python3.10/site-packages/google/colab
--- local_server/run.sh
+++ local_server/run.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+# Checkout the version
+cd /root/.cache/SaprotHub/SaprotHub
+local=$(git rev-parse HEAD)
+remote=$(git ls-remote https://github.com/westlake-repl/SaprotHub.git | grep HEAD)
+remote=(${remote//,/ }[0])
+if [ "$local" != "$remote" ]; then
+    echo "The version is not the latest. Updating..."
+    git pull
+    pip install -r ./requirements.txt
+
+fi
+local=$(git rev-parse HEAD)
+
+# Run the server
 source activate SaprotHub
 jupyter notebook --config ./jupyter_notebook_config.py
