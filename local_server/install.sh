5272585137f1460c946b340ceb1d434834de15cb
LTEnjoy <sujinltenjoy@gmail.com>
2024-12-23 15:00:27 +0800

    Upload

--- local_server/install.sh
+++ local_server/install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-conda create -n SaprotHub python=3.10 --yes
+conda create -n SaprotHub python=3.10.15 --yes
 source activate SaprotHub
 
 mkdir -p /root/.cache/SaprotHub
+
+apt-get -y install zip
 
 # Install packages
 git clone https://github.com/googlecolab/colabtools.git /root/.cache/SaprotHub/colabtools
 pip install /root/.cache/SaprotHub/colabtools/
 pip install -r ./requirements.txt
 
-# Overwrite files for properly calling colab
+# Overwrite files to properly call colab
 colab_dir=$CONDA_PREFIX/lib/python3.10/site-packages/google/colab
 cp ./data_table.py $colab_dir/data_table.py
 cp ./_reprs.py $colab_dir/_reprs.py
