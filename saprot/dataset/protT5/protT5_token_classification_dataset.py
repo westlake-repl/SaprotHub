a383b465579274e710b13dd37d1a6c79058adfd7
Xuting Zhang <140240700+lxzcpro@users.noreply.github.com>
2025-07-11 00:15:46 +0800

    Update protT5_token_classification_dataset.py

--- saprot/dataset/protT5/protT5_token_classification_dataset.py
+++ saprot/dataset/protT5/protT5_token_classification_dataset.py
@@ -1,19 +1,14 @@
 import json
-import random
 import torch
-
 from ..data_interface import register_dataset
 from transformers import T5Tokenizer
 from ..lmdb_dataset import *
-from data.data_transform import pad_sequences
-import re
-
 
 @register_dataset
 class ProtT5TokenClassificationDataset(LMDBDataset):
     def __init__(self,
                  tokenizer: str,
-                 max_length: int = 1024,
+                 max_length: int = 512,
                  **kwargs):
         """
         Args:
@@ -24,17 +19,12 @@ class ProtT5TokenClassificationDataset(LMDBDataset):
         super().__init__(**kwargs)
         self.tokenizer = T5Tokenizer.from_pretrained(tokenizer)
         self.max_length = max_length
+        self.ignore_index = -100
 
     def __getitem__(self, index):
         entry = json.loads(self._get(index))
-        seq = entry['seq'][::2]
-
-        # Add 0 to the end of the label to ignore the cls token
-        if len(entry["label"]) >= self.max_length:
-            label = entry["label"][:self.max_length - 1] + [0]
-        else:
-            label = entry["label"][:self.max_length] + [0]  
-        label = torch.tensor(label, dtype=torch.long)
+        seq = entry['seq'][::2] 
+        label = entry["label"]
         
         return seq, label
 
@@ -42,15 +32,30 @@ class ProtT5TokenClassificationDataset(LMDBDataset):
         return int(self._get("length"))
 
     def collate_fn(self, batch):
-        seqs, label_ids = tuple(zip(*batch))
-        # Add a space between each amino acid
-        seqs = tuple(" ".join(seq) for seq in seqs)
-        # Pad the label_ids with 0
-        label_ids = pad_sequences(label_ids, constant_value=0)
-        labels = {"labels": label_ids}
+        seqs, raw_labels = tuple(zip(*batch))
         
-        # Encode the sequences
-        encoder_info = self.tokenizer.batch_encode_plus(seqs, padding=True, truncation=True, return_tensors='pt', max_length=self.max_length)
-        inputs = {"inputs": encoder_info}
+        # Prepare sequences for ProtT5 tokenizer (add spaces)
+        seqs_with_spaces = tuple(" ".join(seq) for seq in seqs)
+        
+        inputs_encoding = self.tokenizer.batch_encode_plus(
+            seqs_with_spaces, 
+            padding='max_length', 
+            truncation=True, 
+            return_tensors='pt', 
+            max_length=self.max_length
+        )
 
-        return inputs, labels
+        target_length = inputs_encoding['input_ids'].shape[1]
+
+        processed_labels = []
+        for label_list in raw_labels:
+            truncated_label_list = label_list[:target_length - 1]
+
+            padding_size = target_length - len(truncated_label_list)
+            padded_label_list = truncated_label_list + [self.ignore_index] * padding_size
+            
+            processed_labels.append(padded_label_list)
+
+        labels_tensor = torch.tensor(processed_labels, dtype=torch.long)
+        
+        return {"inputs": inputs_encoding}, {"labels": labels_tensor}
