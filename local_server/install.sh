7dcd07f0194084e0532c3d570a6884c381c991d0
LTEnjoy <sujinltenjoy@gmail.com>
2026-06-24 19:08:23 +0800

    Fix the version of the colabtools package

--- local_server/install.sh
+++ local_server/install.sh
@@ -6,9 +6,15 @@ mkdir -p /root/.cache/SaprotHub
 
 apt-get -y install zip
 
+local_server_dir=$(pwd)
+
 # Install packages
 git clone https://github.com/googlecolab/colabtools.git /root/.cache/SaprotHub/colabtools
+cd /root/.cache/SaprotHub/colabtools
+git checkout e8519e12f553b0597c0e067cd9e4df821bdc6b2e
 pip install /root/.cache/SaprotHub/colabtools/
+
+cd $local_server_dir
 pip install -r ./requirements.txt
 
 # Overwrite files to properly call colab
